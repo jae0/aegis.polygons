dfc2f2bdcdeb7fe1f0f7ab50f5ec2d699d87f9df
Jae S Choi <choi.jae.seok@gmail.com>
2019-09-27 12:06:39 -0300

    fix names

--- inst/scripts/01.polygons.r
+++ inst/scripts/01.polygons.r
@@ -1,4 +1,36 @@
 
+
+
+
+# ------------------------------------------------
+# Create unerlying areal units for modelling of bathymetry, etc using lattice methods
+
+for ( areal_units_resolution_km in c(10, 20, 25) ) {
+  for ( spatial_domain in c("snowcrab", "SSE")) {
+    areal_units_overlay = "snowcrab_managementareas"
+    if ( spatial_domain=="SSE") areal_units_overlay = "groundfish_strata"
+    p = aegis.bathymetry::bathymetry_parameters(
+      project_class = "carstm", # defines which parameter set to load
+      id = paste("bathymetry", areal_units_overlay, sep="_"),
+      inputdata_spatial_discretization_planar_km = 1,  # 1 km .. requires 32 GB RAM and limit of speed -- controls resolution of data prior to modelling to reduce data set and speed up modelling
+      spatial_domain = spatial_domain,  # defines spatial area
+      areal_units_resolution_km = areal_units_resolution_km, # km dim of lattice
+      areal_units_proj4string_planar_km = projection_proj4string("utm20"),  # coord system to use for areal estimation and gridding for carstm
+      # areal_units_proj4string_planar_km = "+proj=omerc +lat_0=44.0 +lonc=-63.0 +gamma=0.0 +k=1 +alpha=325 +x_0=0 +y_0=0 +ellps=WGS84 +units=km",  # oblique mercator, centred on Scotian Shelf rotated by 325 degrees
+      areal_units_strata_type = "lattice", # "aegis_lattice" to use ageis fields instead of carstm fields ... note variables are not the same
+      areal_units_overlay = areal_units_overlay, # additional polygon layers for subsequent analysis such as management area: "snowcrab" or "groundfish"  # for now ..
+      areal_units_constraint="none", # set[, c("lon", "lat")],  # to limit to sppoly to only those with data that fall into them
+       libs = RLibrary ( "sp", "spdep", "rgeos", "spatialreg", "INLA", "raster", "aegis",  "aegis.polygons", "aegis.bathymetry", "carstm" )
+    )
+    # sppoly = areal_units( p=p, redo=TRUE )
+    M = bathymetry_carstm( p=p, DS="aggregated_data", redo=TRUE )  # will redo if not found .. not used here but used for data matching/lookup in other aegis projects that use bathymetry
+    # M = bathymetry_carstm( p=p, DS="carstm_inputs", redo=TRUE )  # will redo if not found
+  }
+
+}
+
+
+
 # Roll your own polygon via mouse interaction
 
 # Example
