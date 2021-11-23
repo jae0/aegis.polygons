cf25a392d4a4d8643465b83c21e9deb10f19ced0
Jae S Choi <choi.jae.seok@gmail.com>
2021-11-23 08:42:27 -0400

    cleanup

--- R/areal_units.R
+++ R/areal_units.R
@@ -223,7 +223,6 @@ areal_units = function( p=NULL, areal_units_fn_full=NULL, plotit=FALSE, sa_thres
         tus=p$tus,
         fraction_todrop = p$fraction_todrop,
         fraction_cv = p$fraction_cv,  # stopping criterion: when cv drops below this value
-        fraction_good_bad = p$fraction_good_bad,  # stopping criterion: fraction of removal candidates to total increases to this value
         nAU_min = p$nAU_min,   # stoppping criterion: allow no less than this number of areal units
         verbose = verbose
       )  # voroni tesslation and delaunay triagulation
