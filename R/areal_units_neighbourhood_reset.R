89754ee3eb21827d42a92b582cbc505bce8b5c3a
Jae S Choi <choi.jae.seok@gmail.com>
2022-02-04 19:51:19 -0400

    add new reset method

---
+++ R/areal_units_neighbourhood_reset.R
@@ -0,0 +1,27 @@
+areal_units_neighbourhood_reset = function ( sppoly, snap=1 ) {
+  # after modifying polygons, make sure neighbourhoods are consistent 
+  # expected aegis.polygons attributes  
+
+  require(spdep)
+  
+  W.nb = poly2nb(sppoly, row.names=sppoly$AUID, queen=TRUE, snap=snap )  # slow .. ~1hr?
+  W.remove = which(card(W.nb) == 0)
+  if ( length(W.remove) > 0 ) {
+    # remove isolated locations and recreate sppoly .. alternatively add links to W.nb
+    W.keep = which(card(W.nb) > 0)
+    W.nb = nb_remove( W.nb, W.remove )
+    sppoly = sppoly[W.keep,]
+    row.names(sppoly) = sppoly$AUID
+    sppoly = sppoly[order(sppoly$AUID),]
+    sppoly = st_make_valid(sppoly)
+  }
+
+  nb = INLA::inla.read.graph( spdep::nb2mat( W.nb ))
+  attr(nb, "region.id") = sppoly$AUID
+  attr(sppoly, "nb") = nb  # adding neighbourhood as an attribute to sppoly
+  attr(sppoly, "W.nb") = W.nb  # adding neighbourhood as an attribute to sppoly
+
+  return(sppoly)
+}
+
+
