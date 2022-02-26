df5d580136765977ff321536e0e10e6ef9e844c9
Jae S Choi <choijaeseok@gmail.com>
2022-02-25 21:24:55 -0400

    plot tweaks for csas

---
+++ R/polygons_worldHighres.R
@@ -0,0 +1,19 @@
+
+polygons_worldHighres = function( regions="Canada", xlim=c(-70,-59), ylim=c(40, 50) ) {
+
+  require(mapdata)
+  require(map)
+  require(sf)
+
+  o = sf::st_as_sf(maps::map("world", regions=regions, plot = FALSE, fill = TRUE))
+  st_crs(o) = st_crs( "epsg:4326" )
+
+  xy = as.matrix( expand.grid( xlim, ylim) )
+  mp = st_multipoint( xy )
+  mp = st_bbox (mp )
+  bbox =  st_as_sfc( st_bbox( mp ) )
+  st_crs(bbox) = st_crs( "epsg:4326" )
+  o = st_difference( bbox, o )
+
+  return(o)
+}
