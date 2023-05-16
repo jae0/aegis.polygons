11445f73428140badcf3a4dca0484ccea94426d7
Jae S Choi <choijaeseok@gmail.com>
2023-05-16 17:50:05 -0300

    update to new carstm structure

--- R/maritimes_groundfish_strata.R
+++ R/maritimes_groundfish_strata.R
@@ -31,7 +31,7 @@ maritimes_groundfish_strata = function( NB_graph=NULL, areal_units_timeperiod="p
       st_geometry(groundfish_strata[40,]) = st_difference( st_geometry(groundfish_strata[40,]), st_geometry(groundfish_strata[38,]))
       groundfish_strata = st_make_valid(groundfish_strata)
 
-      attr(groundfish_strata, "space.id") = as.character( groundfish_strata[,"AUID"] )
+      attr(groundfish_strata, "space_id") = as.character( groundfish_strata[,"AUID"] )
 
       if (0) {
         plot(st_geometry( groundfish_strata) )
@@ -52,7 +52,7 @@ maritimes_groundfish_strata = function( NB_graph=NULL, areal_units_timeperiod="p
 
       # i = which( ! st_is_valid(groundfish_strata) )
       groundfish_strata = st_make_valid(groundfish_strata)
-      attr(groundfish_strata, "space.id") = as.character( groundfish_strata[,"AUID"] )
+      attr(groundfish_strata, "space_id") = as.character( groundfish_strata[,"AUID"] )
 
       return( groundfish_strata)
     }
