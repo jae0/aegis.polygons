e92332483786edd207f24ec2da806e70b3bb1e09
Jae S Choi <choijaeseok@gmail.com>
2023-05-17 10:02:23 -0300

    tidy

--- inst/scripts/01_polygons.r
+++ inst/scripts/01_polygons.r
@@ -3,32 +3,32 @@
 
 
 # ------------------------------------------------
-# Create unerlying areal units for modelling of bathymetry, etc using lattice methods via carstm
+# Create underlying areal units for modelling of bathymetry, etc using lattice methods via carstm
 
-require(aegis.bathymetry)
+# require(aegis.bathymetry)
 
-for ( areal_units_resolution_km in c(5, 10, 20, 25) ) {
-  # for ( spatial_domain in c("snowcrab", "SSE")) {
-   for ( spatial_domain in c("snowcrab", "SSE")) {
-    areal_units_overlay = "snowcrab_managementareas"
-    if ( spatial_domain=="SSE") areal_units_overlay = "none"
-    p = bathymetry_parameters(
-      project_class = "carstm", # defines which parameter set to load
-      inputdata_spatial_discretization_planar_km = 1,  # 1 km .. requires 32 GB RAM and limit of speed -- controls resolution of data prior to modelling to reduce data set and speed up modelling
-      spatial_domain = spatial_domain,  # defines spatial area
-      areal_units_resolution_km = areal_units_resolution_km, # km dim of lattice
-      areal_units_proj4string_planar_km = projection_proj4string("utm20"),  # coord system to use for areal estimation and gridding for carstm
-      # areal_units_proj4string_planar_km = "+proj=omerc +lat_0=44.0 +lonc=-63.0 +gamma=0.0 +k=1 +alpha=325 +x_0=0 +y_0=0 +ellps=WGS84 +units=km",  # oblique mercator, centred on Scotian Shelf rotated by 325 degrees
-      areal_units_type = "lattice", # "stmv_fields" to use ageis fields instead of carstm fields ... note variables are not the same
-      areal_units_overlay = areal_units_overlay, # additional polygon layers for subsequent analysis such as management area: "snowcrab" or "groundfish"  # for now ..
-      libs = RLibrary ( "sp", "rgeos", "INLA", "raster", "aegis",  "aegis.polygons", "aegis.bathymetry", "carstm" )
-    )
+# for ( areal_units_resolution_km in c(5, 10, 20, 25) ) {
+#   # for ( spatial_domain in c("snowcrab", "SSE")) {
+#    for ( spatial_domain in c("snowcrab", "SSE")) {
+#     areal_units_overlay = "snowcrab_managementareas"
+#     if ( spatial_domain=="SSE") areal_units_overlay = "none"
+#     p = bathymetry_parameters(
+#       project_class = "carstm", # defines which parameter set to load
+#       inputdata_spatial_discretization_planar_km = 1,  # 1 km .. requires 32 GB RAM and limit of speed -- controls resolution of data prior to modelling to reduce data set and speed up modelling
+#       spatial_domain = spatial_domain,  # defines spatial area
+#       areal_units_resolution_km = areal_units_resolution_km, # km dim of lattice
+#       areal_units_proj4string_planar_km = projection_proj4string("utm20"),  # coord system to use for areal estimation and gridding for carstm
+#       # areal_units_proj4string_planar_km = "+proj=omerc +lat_0=44.0 +lonc=-63.0 +gamma=0.0 +k=1 +alpha=325 +x_0=0 +y_0=0 +ellps=WGS84 +units=km",  # oblique mercator, centred on Scotian Shelf rotated by 325 degrees
+#       areal_units_type = "lattice", # "stmv_fields" to use ageis fields instead of carstm fields ... note variables are not the same
+#       areal_units_overlay = areal_units_overlay, # additional polygon layers for subsequent analysis such as management area: "snowcrab" or "groundfish"  # for now ..
+#       libs = RLibrary ( "sp", "rgeos", "INLA", "raster", "aegis",  "aegis.polygons", "aegis.bathymetry", "carstm" )
+#     )
 
-    sppoly = areal_units( p=p, redo=TRUE )
+#     sppoly = areal_units( p=p, redo=TRUE )
 
-  }
+#   }
 
-}
+# }
 
 
 
